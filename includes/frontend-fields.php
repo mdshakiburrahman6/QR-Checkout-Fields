22bfdcf263bcec6c966e28db895df62e31414d30
Md Shakibur Rahman <mdshakiburrahman6@gmail.com>
2026-01-21 10:34:21 +0600

    Fix frontend css

--- includes/frontend-fields.php
+++ includes/frontend-fields.php
@@ -8,9 +8,9 @@ add_action('pmpro_checkout_after_username', function () {
 
     <fieldset>
         <legend>I am</legend>
-        <label><input type="radio" name="qr_identity" value="driver" required> Driver</label><br>
-        <label><input type="radio" name="qr_identity" value="owner"> Owner</label><br>
-        <label><input type="radio" name="qr_identity" value="driver_owner"> Driver & Owner</label>
+        <label class="qr_role"><input type="radio" name="qr_identity" value="driver" required> Driver</label><br>
+        <label class="qr_role"><input type="radio" name="qr_identity" value="owner"> Owner</label><br>
+        <label class="qr_role"><input type="radio" name="qr_identity" value="driver_owner"> Driver & Owner</label>
     </fieldset>
 
     <?php if (!empty($config['global'])): ?>
